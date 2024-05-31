5fe3d8f3473e4b6337170f3a24386a445e4f09be
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-31 10:43:54 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -56,9 +56,7 @@ except ImportError as e:
     logger.error(f"Error importing dashboard module: {e}")
     raise
 
-# Run the Flask application
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+
 
 
 
